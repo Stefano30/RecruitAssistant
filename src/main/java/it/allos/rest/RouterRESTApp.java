76d97aa6c373bbe446bba74fc3b1f994ba1a5a24
tritonim <tritonim@1912-tritonim.allos.local>
2020-07-16 15:44:31 +0200

    aggiunto JacksonJsonProvider per REST API

--- src/main/java/it/allos/rest/RouterRESTApp.java
+++ src/main/java/it/allos/rest/RouterRESTApp.java
@@ -1,18 +1,18 @@
 package it.allos.rest;
 
-import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
-import javax.ws.rs.core.Application;
 import javax.ws.rs.core.MediaType;
 
-@ApplicationPath("/watson/api")
-public class RouterRESTApp extends Application {
+@Path("/watson/api")
+public class RouterRESTApp {
+	
     @GET
     @Path("/prova")
     @Produces(MediaType.TEXT_PLAIN)
     public String get() {
-        return "Hello World!";
+        return "Ciaoneeee";
     }
+    
 }
