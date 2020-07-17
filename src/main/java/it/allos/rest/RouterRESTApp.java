90bcc069736eaf6807cfb83ba1d2d1ce55fff3b1
Stefano Cavaliere <stefano.cavaliere.1@studenti.unipd.it>
2020-07-17 15:46:22 +0200

    correzioni i RouterREST

--- src/main/java/it/allos/rest/RouterRESTApp.java
+++ src/main/java/it/allos/rest/RouterRESTApp.java
@@ -13,6 +13,7 @@ import com.ibm.watson.assistant.v2.Assistant;
 import com.ibm.watson.assistant.v2.model.MessageInput;
 import com.ibm.watson.assistant.v2.model.MessageOptions;
 import com.ibm.watson.assistant.v2.model.MessageResponse;
+import com.ibm.watson.assistant.v2.model.RuntimeIntent;
 import com.ibm.watson.assistant.v2.model.RuntimeResponseGeneric;
 
 import it.allos.dto.Message;
@@ -33,14 +34,27 @@ public class RouterRESTApp {
     @Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.APPLICATION_JSON)
     public Message request(Message request) {
+
         Assistant service = SingleAssistant.getAssistant();
         String sessionId = SingleAssistant.getSessionID();
+        Message returnMessage;
+
         //invio messaggio
         MessageInput input = new MessageInput.Builder().messageType("text").text(request.getText()).build();
         MessageOptions messageOptions = new MessageOptions.Builder(SingleAssistant.ASSISTANT_ID, sessionId).input(input).build();
+
         //risposta
         MessageResponse response = service.message(messageOptions).execute().getResult();
         List<RuntimeResponseGeneric> responseGeneric = response.getOutput().getGeneric();
+        if (responseGeneric.get(0).responseType().equals("text")) {
+            returnMessage = new Message(responseGeneric.get(0).text());
+        }
+
+        //determino se la conversazione è finita
+        List<RuntimeIntent> responseIntents = response.getOutput().getIntents();
+        if(responseIntents.size() > 0 && responseIntents.get(0).intent() == "General_Ending") {
+            SingleAssistant.deleteSession();
+        }
         return new Message(responseGeneric.get(0).text());
     }
 
--- src/main/java/it/allos/watson/SingleAssistant.java
+++ src/main/java/it/allos/watson/SingleAssistant.java
@@ -10,7 +10,7 @@ public class SingleAssistant {
     public static final String ASSISTANT_ID = "b68d1fb2-35ca-415f-893e-8134484f11f9";
     public static final String API_KEY = "sYJ55YEoz8CDvulCnL0aFOsCF6VG_mFFHKx1Zbv2ST8b";
     public static final String SERVICE_URL = "https://api.eu-gb.assistant.watson.cloud.ibm.com/instances/8406380e-ddb7-4976-9b64-2d26cebbfd12";
-    public static final String API_VERSION = "2020/07/17";
+    public static final String API_VERSION = "2020-07-17";
 
     private static Assistant assistantInstance = null;
     private static String sessionID = null;
